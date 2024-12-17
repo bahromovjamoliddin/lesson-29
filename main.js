ceb957332cecc03da919a64f8334f5738a8aabbb
baxromovjamoliddin <bahromovjamoliddin0077@gmail.com>
2024-12-17 18:58:27 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const userProfile = document.querySelector(".user_contener")
+ setTimeout(() => {
+    userProfile.classList.toggle("active");
+  }, 5000);
+  
+  setTimeout(() => {
+    userProfile.classList.toggle("active");
+  }, 15000);
